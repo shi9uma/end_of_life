5b3708ee06550065388d108c8118e8260f24c40b
wkyuu <tataramoriko@163.com>
2024-04-09 16:06:07 +0800

    update

--- create_password.sh
+++ create_password.sh
@@ -1,46 +1,41 @@
 #!/bin/bash
 
-# 显示字符集
-showCharSet() {
-    echo "string.ascii_letters => abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    echo "string.digits => 0123456789"
+generate_password() {
+    local length="$1"
+    local seed="$2"
+
+    if [ -z "$length" ]; then
+        length=15
+        echo "default key length is 15"
+    fi
+
+    if [ -z "$seed" ]; then
+        echo "Seed is required to generate a deterministic password."
+        return 1
+    fi
+
+    echo -n "$seed" | openssl dgst -sha256 -binary | openssl base64 | tr -dc 'a-zA-Z0-9-#.' | head -c $length
     echo
 }
 
-# 生成随机字符串函数
-func() {
-    local length="$1"
-    local seed="$2"
-    local charSet="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-#."
-    
-    # 如果长度未指定，默认为15
-    if [ -z "$length" ]; then
-        length=15
-    fi
-    
-    # 如果种子未指定，使用当前时间戳
-    if [ -z "$seed" ]; then
-        seed=$(date +%s)
-    fi
-    
-    # 设置随机种子
-    RANDOM=$seed
-    
-    local result=""
-    local charSetLength=${#charSet}
-    for (( i=0; i<$length; i++ )); do
-        local rand=$(( RANDOM % charSetLength ))
-        result="${result}${charSet:$rand:1}"
-    done
-    
-    echo "$result"
-}
+while [[ $# -gt 0 ]]; do
+    key="$1"
 
-# 主执行逻辑
-if [ "$1" == "show" ]; then
-    showCharSet
-else
-    length=${1:-32} # 如果未指定长度，默认为32
-    seed=$2
-    func "$length" "$seed"
-fi
+    case $key in
+        --length)
+        length="$2"
+        shift # past argument
+        shift # past value
+        ;;
+        --key)
+        seed="$2"
+        shift # past argument
+        shift # past value
+        ;;
+        *)    # unknown option
+        shift # past argument
+        ;;
+    esac
+done
+
+generate_password "$length" "$seed"
