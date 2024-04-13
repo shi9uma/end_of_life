e99ac66aab9e0a4f66be908cdf81059baf43b124
wkyuu <wkyuu@majo.im>
2024-04-13 22:59:06 +0800

    update scripts

--- backups/backup.ps1
+++ backups/backup.ps1
@@ -1,7 +1,7 @@
-# base path config
-$src_dir = "E:\Documents\markdown\02_md"
-$dst_root_dir = "E:\project\14_end_of_life\backups"
-$dst_dir = "$dst_root_dir\blogs"
+# base path configs
+$src_dir = "E:/Documents/markdown/02_md"
+$dst_root_dir = "E:/project/14_end_of_life/backups"
+$dst_dir = "$dst_root_dir/blogs"
 
 # args
 if ($args.Count -ge 1) {
@@ -11,15 +11,43 @@ if ($args.Count -ge 2) {
     $dst_dir = $args[1]
 }
 
+# before backup list
+$initialFiles = Get-ChildItem -Path $dst_dir -Recurse -File | Select-Object -ExpandProperty FullName
+
 # rel path
 $script_path = Split-Path -Parent $MyInvocation.MyCommand.Definition
-$end_of_life_py_path = Join-Path $script_path "..\end_of_life.py"
+$end_of_life_py_path = Join-Path $script_path "../end_of_life.py"
 
 # exec
 & python $end_of_life_py_path `
 -i $src_dir `
 -o $dst_dir `
 -r `
--k "$dst_root_dir\key" `
--s "$dst_root_dir\salt" `
+-k "$dst_root_dir/key" `
+-s "$dst_root_dir/salt" `
 enc
+
+# check new file
+$currentFiles = Get-ChildItem -Path $dst_dir -Recurse -File | Select-Object -ExpandProperty FullName
+$newFiles = $currentFiles | Where-Object { $_ -notin $initialFiles }
+if ($newFiles) {
+    Write-Host "New files added:"
+    $newFiles | ForEach-Object { Write-Host $_.Replace("$dst_root_dir/", "") -ForegroundColor Green }
+}
+
+# push time check
+$currentTime = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+$backupTimePath = "$dst_root_dir/backup_time"
+if (Test-Path $backupTimePath) {
+    $lastBackupTime = Get-Content $backupTimePath
+    $lastBackupDateTime = [DateTime]::ParseExact($lastBackupTime, "yyyy-MM-dd HH:mm:ss", $null)
+    $elapsedDays = (New-TimeSpan -Start $lastBackupDateTime -End $currentTime).Days
+    if ($elapsedDays -gt 30) {
+        Write-Host "Warning: It has been over 30 days since the last push!" -ForegroundColor Red
+    }
+} else {
+    Write-Host "Creating initial backup time stamp file." -ForegroundColor Green
+    Set-Content -Path $backupTimePath -Value $currentTime
+}
+
+Write-Host "Backup process completed." -ForegroundColor Green
--- backups/backup.sh
+++ backups/backup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# base config
-src_dir=/e/Documents/markdown/02_md
-dst_root_dir=/e/project/14_end_of_life/backups
-dst_dir=$dst_root_dir/blogs
+# Base path configs
+src_dir="/e/Documents/markdown/02_md"
+dst_root_dir="/e/project/14_end_of_life/backups"
+dst_dir="$dst_root_dir/blogs"
 
-# args
+# Args
 if [ $# -ge 1 ]; then
     src_dir=$1
 fi
@@ -13,15 +13,44 @@ if [ $# -ge 2 ]; then
     dst_dir=$2
 fi
 
-# rel path
-script_path=$(cd "$(dirname "$0")" && pwd)
-end_of_life_py_path=$script_path/../end_of_life.py
+# Before backup list
+initial_files=$(find $dst_dir -type f)
 
-# exec
+# Rel path
+script_path=$(dirname "$0")
+end_of_life_py_path="$script_path/../end_of_life.py"
+
+# Exec
 python $end_of_life_py_path \
--i $src_dir \
--o $dst_dir \
+-i "$src_dir" \
+-o "$dst_dir" \
 -r \
--k $dst_root_dir/key \
--s $dst_root_dir/salt \
+-k "$dst_root_dir/key" \
+-s "$dst_root_dir/salt" \
 enc
+
+# check new file
+current_files=$(find $dst_dir -type f)
+new_files=$(comm -13 <(echo "$initial_files") <(echo "$current_files"))
+if [ -n "$new_files" ]; then
+    echo "New files added:"
+    echo "$new_files" | sed "s|$dst_root_dir/||g" | awk '{print $0}' | GREP_COLOR='01;32' grep --color=always "^"
+fi
+
+# push time check
+current_time=$(date "+%Y-%m-%d %H:%M:%S")
+backup_time_path="$dst_root_dir/backup_time"
+if [ -f "$backup_time_path" ]; then
+    last_backup_time=$(cat "$backup_time_path")
+    last_backup_datetime=$(date -d "$last_backup_time" +%s)
+    current_datetime=$(date -d "$current_time" +%s)
+    elapsed_days=$(( ($current_datetime - $last_backup_datetime) / 86400 ))
+    if [ $elapsed_days -gt 30 ]; then
+        echo -e "\033[0;31mWarning: It has been over 30 days since the last push!\033[0m"
+    fi
+else
+    echo -e "\033[0;32mCreating initial backup time stamp file.\033[0m"
+    echo "$current_time" > "$backup_time_path"
+fi
+
+echo -e "\033[0;32mBackup process completed.\033[0m"
