de983710dd0b5081addeb7f0935bc47cf7e65233
shiguma <wkyuu@majo.im>
2024-05-28 15:42:45 +0800

    :wq1xia

---
+++ backups/scripts/mac_restore.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env zsh
+
+# Base path configurations
+script_path=$(dirname "$0")
+end_of_life_py_path="$script_path/../../end_of_life.py"
+
+src_root_dir="$script_path/../blogs"
+dst_root_dir="$HOME/repo/markdown"
+
+restore_main() {
+    src_dir=$1
+    dst_dir=$2
+    # Execute Python script
+    python3 \
+        "$end_of_life_py_path" \
+        -i "$src_dir" \
+        -o "$dst_dir" \
+        -r \
+        -k "$script_path/../key" \
+        -s "$script_path/../salt" \
+        dec
+}
+
+echo $src_root_dir $dst_root_dir
+restore_main "$src_root_dir" "$dst_root_dir"
